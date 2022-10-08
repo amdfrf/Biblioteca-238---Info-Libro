18cad4d7f662071278a742c2d3615220dc6d1259
Lihuén <110670239+lud-lihuen@users.noreply.github.com>
2022-10-08 11:21:21 -0300

    Imágenes y comentarios

--- scripts/ver-libro.js
+++ scripts/ver-libro.js
@@ -3,31 +3,39 @@ let contenido = document.getElementById("contenido");
 let imagenes = document.getElementById("imagenes");
 
 
+function mostrar_infolibro() {
 
-function mostrar_infolibro(){
-
+    // Agrego informacion del libro al html:
     let htmlcontentoAppend = "";
-
-        htmlcontentoAppend += `
-        
-            <h3>${info_libro.titulo} </h3>
-            <p class="ml-5 text-muted">${info_libro.autor}</p>
-            <hr>
-            <p class="text-muted infoAd">${info_libro.isbn}</p>
-            <p class="text-muted infoAd">${info_libro.editorial} </p>
-            <p class="text-muted infoAd">${info_libro.paginas} páginas</p>
-            <hr>
-            <p>${info_libro.descripcion}</p>`
-
+    htmlcontentoAppend += `
+        <h3>${info_libro.titulo}</h3>
+        <h5 class="text-muted">${info_libro.autor}</h5>
+        <hr>
+        <p class="text-muted">${info_libro.isbn}<br>
+        ${info_libro.editorial}<br>
+        ${info_libro.paginas} páginas</p>
+        <hr>
+        <p>${info_libro.descripcion}</p>
+        <hr>
+        `
     contenido.innerHTML += htmlcontentoAppend;
 
+    // Agrego imagenes del libro al html:
+    let images = "";
+    for (let imagen of info_libro.imagenes) {
+        images += `
+        <img height="200" src="${imagen}" alt="${imagen}">
+        `;
+    }
+    imagenes.innerHTML += images;
 }
 
 
 document.addEventListener("DOMContentLoaded", function(){
 
-        getJSONData(LIBRO_URL + localStorage.getItem("libroID") +".json").then(resultado => {
-            if (resultado.status == "ok") {
+    getJSONData(LIBRO_URL + localStorage.getItem("libroID") +".json").then(resultado => {
+        
+        if (resultado.status == "ok") {
             info_libro = resultado.data;
             mostrar_infolibro();
         } 
